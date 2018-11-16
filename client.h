a08a9b69e00bdd7282d1d09c087aa22eba5a6966
sebangi <sebastien_angibaud@yahoo.fr>
2018-11-16 13:09:04 +0100

    Ajout des fichiers

---
+++ client.h
@@ -0,0 +1,28 @@
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QTcpSocket>
+#include <QObject>
+
+class QTcpSocket;
+class QNetworkSession;
+
+class Client : public QObject
+{
+    Q_OBJECT
+
+public:
+    Client();
+
+private slots:
+    void lireTexte();
+    void afficherErreur(QAbstractSocket::SocketError socketError);
+
+private:
+    QTcpSocket *m_tcpSocket;
+    quint16 m_blockSize;
+    QNetworkSession *m_networkSession;
+};
+
+#endif
